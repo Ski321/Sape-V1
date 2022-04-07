0a9d10a077f2cdac00a57286d24a2531bb3b0f34
Ski321 <103225231+Ski321@users.noreply.github.com>
2022-04-07 17:42:38 -0600

    Create sapeMainScript.lua

---
+++ sapeMainScript.lua
@@ -0,0 +1,11 @@
+shared.VapeIndependent = true
+shared.CustomSaveVape = "lol"
+local uilib = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+local combat = uilib["ObjectsThatCanBeSaved"]["CombatWindow"]["Api"]
+local esp = combat.CreateOptionsButton({
+    Name = "lololol",
+    Function = function()
+    end
+})
+shared.VapeManualLoad = true
